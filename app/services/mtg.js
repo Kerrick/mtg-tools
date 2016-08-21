6521db9f1a94ff7cf666f885b191f872d9c3fd1b
Kerrick Long <me@kerricklong.com>
2016-08-20 22:04:17 -0500

    Load MTGJson over SSL

--- app/services/mtg.js
+++ app/services/mtg.js
@@ -4,7 +4,7 @@ export default Ember.Service.extend({
   allSets: Ember.computed(function() {
     return Ember.ObjectProxy.extend(Ember.PromiseProxyMixin).create({
       promise: Ember.RSVP.resolve(Ember.$.ajax({
-        url: 'http://mtgjson.com/json/AllSets-x.jsonp',
+        url: 'https://mtgjson.com/json/AllSets-x.jsonp',
         dataType: 'jsonp',
         cache: true,
         jsonpCallback: 'mtgjsoncallback'
