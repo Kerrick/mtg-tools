0ba5dd7f4ac192ee66e7c08476c725d4e1cf5eac
Kerrick Long <me@kerricklong.com>
2016-08-21 01:17:37 -0500

    Add Basic Lands toggle

--- app/controllers/playtest-cards/print.js
+++ app/controllers/playtest-cards/print.js
@@ -4,6 +4,7 @@ import deckParser from 'mtg-tools/utils/deck-parser';
 export default Ember.Controller.extend({
   mtg: Ember.inject.service(),
   playtestCards: Ember.inject.controller(),
+  showBasicLands: true,
   mtgJsonCards: Ember.computed('playtestCards.decklist', function() {
     const decklist = deckParser(this.get('playtestCards.decklist'));
     return decklist.cards.concat(decklist.sideboard).map(card => {
@@ -16,6 +17,14 @@ export default Ember.Controller.extend({
   }),
   desiredCards: Ember.computed('mtgJsonCards.[]', function() {
     return this.get('mtgJsonCards').reject(card => Ember.isEmpty(card.printings));
+  }),
+  desiredCardsWithoutBasicLands: Ember.computed('desiredCards', function() {
+    return this.get('desiredCards').reject(card => {
+      return card.printings[0].types.contains('Land') &&
+        card.printings[0].supertypes &&
+        card.printings[0].supertypes.contains('Basic')
+      ;
+    });
   }),
   problemCards: Ember.computed('mtgJsonCards.[]', function() {
     return this.get('mtgJsonCards').filter(card => Ember.isEmpty(card.printings));
