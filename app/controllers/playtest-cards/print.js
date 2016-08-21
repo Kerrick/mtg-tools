5c43302f5d85317356029cda613ad64f193c76ad
Kerrick Long <me@kerricklong.com>
2016-08-20 21:36:05 -0500

    Basic playtest card capabilities

--- app/controllers/playtest-cards/print.js
+++ app/controllers/playtest-cards/print.js
@@ -1,5 +1,23 @@
 import Ember from 'ember';
+import deckParser from 'playtest-cards/utils/deck-parser';
 
 export default Ember.Controller.extend({
-  playtestCards: Ember.inject.controller()
+  mtg: Ember.inject.service(),
+  playtestCards: Ember.inject.controller(),
+  mtgJsonCards: Ember.computed('playtestCards.decklist', function() {
+    const decklist = deckParser(this.get('playtestCards.decklist'));
+    return decklist.cards.concat(decklist.sideboard).map(card => {
+      return {
+        count: card.number,
+        name: card.name,
+        printings: this.get('mtg').cardsNamed(card.name)
+      };
+    });
+  }),
+  desiredCards: Ember.computed('mtgJsonCards.[]', function() {
+    return this.get('mtgJsonCards').reject(card => Ember.isEmpty(card.printings));
+  }),
+  problemCards: Ember.computed('mtgJsonCards.[]', function() {
+    return this.get('mtgJsonCards').filter(card => Ember.isEmpty(card.printings));
+  })
 });
--- app/services/mtg.js
+++ app/services/mtg.js
@@ -12,4 +12,17 @@ export default Ember.Service.extend({
     });
   }),
   isLoading: Ember.computed.readOnly('allSets.isPending'),
+  cardsNamed(name) {
+    const cards = [];
+    Object.keys(this.get('allSets.content')).filter(setCode => {
+      const found = this.get(`allSets.content.${setCode}.cards`).find(card => card.name === name);
+      if (!found || !found.multiverseid) { return false; }
+      const card = Ember.copy(found, true);
+      card.set = Ember.copy(this.get(`allSets.content.${setCode}`), false);
+      delete card.set.cards;
+      cards.push(card);
+      return true;
+    });
+    return cards;
+  }
 });
